23de1ff47edb34e592b392ae7c7daece0f486994
Gustavo Carvalho <gcrodrigues413@gmail.com>
2020-09-03 09:21:43 -0300

    Development of Delete, Post and Get requests

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories')
+      .then(res => {
+        setRepositories(res.data)
+      })
+      .catch(err => console.log(err))
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      "title": "Contact book",
+      "url": "https://github.com/gcrodrigues/contact-book",
+      "techs": [
+        "React Native",
+        "TypeScript",
+        "NodeJS"
+      ],
+      "likes": 0
+    })
+
+    console.log(response.data)
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`/repositories/${id}`);
+    setRepositories(repositories.filter(
+      repository => repository.id !== id
+    ))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo => (
+          <li key={repo.id}>
+            {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
           </button>
-        </li>
+          </li>
+
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
